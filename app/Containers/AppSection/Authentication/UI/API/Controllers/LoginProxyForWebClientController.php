6927c66c31299c3fb928b626a0ea63af9fee6b5e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-09-22 13:57:36 +0330

    [WIP]

--- app/Containers/AppSection/Authentication/UI/API/Controllers/LoginProxyForWebClientController.php
+++ app/Containers/AppSection/Authentication/UI/API/Controllers/LoginProxyForWebClientController.php
@@ -3,13 +3,28 @@
 namespace App\Containers\AppSection\Authentication\UI\API\Controllers;
 
 use App\Containers\AppSection\Authentication\Actions\ApiLoginProxyForWebClientAction;
+use App\Containers\AppSection\Authentication\UI\API\Documentation\Parameters\LoginProxyForWebClient;
 use App\Containers\AppSection\Authentication\UI\API\Requests\LoginProxyPasswordGrantRequest;
 use App\Containers\AppSection\Authentication\UI\API\Transformers\TokenTransformer;
+use App\Containers\AppSection\User\UI\API\Documentation\Responses\UserTransformerResponse;
 use App\Ship\Parents\Controllers\ApiController;
 use Illuminate\Http\JsonResponse;
+use Vyuldashev\LaravelOpenApi\Attributes\Collection;
+use Vyuldashev\LaravelOpenApi\Attributes\Operation;
+use Vyuldashev\LaravelOpenApi\Attributes\Parameters;
+use Vyuldashev\LaravelOpenApi\Attributes\PathItem;
+use Vyuldashev\LaravelOpenApi\Attributes\Response;
 
+#[PathItem]
 class LoginProxyForWebClientController extends ApiController
 {
+    /**
+     * Login
+     */
+    #[Operation(tags: ['user'], deprecated: true)]
+    #[Response(factory: UserTransformerResponse::class)]
+    #[Collection(['private', 'public'])]
+    #[Parameters(factory: LoginProxyForWebClient::class)]
     public function __invoke(LoginProxyPasswordGrantRequest $request, ApiLoginProxyForWebClientAction $action): JsonResponse
     {
         $result = $action->run($request);
--- app/Containers/AppSection/User/UI/API/Controllers/UpdateUserController.php
+++ app/Containers/AppSection/User/UI/API/Controllers/UpdateUserController.php
@@ -3,12 +3,30 @@
 namespace App\Containers\AppSection\User\UI\API\Controllers;
 
 use App\Containers\AppSection\User\Actions\UpdateUserAction;
+use App\Containers\AppSection\User\UI\API\Documentation\Parameters\UpdateUser;
+use App\Containers\AppSection\User\UI\API\Documentation\Responses\UserTransformerResponse;
+use App\Containers\AppSection\User\UI\API\Documentation\SecuritySchemes\BearerTokenSecurityScheme;
 use App\Containers\AppSection\User\UI\API\Requests\UpdateUserRequest;
 use App\Containers\AppSection\User\UI\API\Transformers\UserTransformer;
 use App\Ship\Parents\Controllers\ApiController;
+use Vyuldashev\LaravelOpenApi\Attributes\Collection;
+use Vyuldashev\LaravelOpenApi\Attributes\Operation;
+use Vyuldashev\LaravelOpenApi\Attributes\Parameters;
+use Vyuldashev\LaravelOpenApi\Attributes\PathItem;
+use Vyuldashev\LaravelOpenApi\Attributes\Response;
 
+#[PathItem]
 class UpdateUserController extends ApiController
 {
+    /**
+     * Update users
+     *
+     * Description for body
+     */
+    #[Operation(security: BearerTokenSecurityScheme::class)]
+    #[Parameters(factory: UpdateUser::class)]
+    #[Response(factory: UserTransformerResponse::class)]
+    #[Collection(['private'])]
     public function __invoke(UpdateUserRequest $request, UpdateUserAction $action): array
     {
         $request->mapInput([
